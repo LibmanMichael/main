14690a459974fa4f7644cd117993f7ee558d7fb6
Michael Libman <lcdstudio@yandex.ru>
2018-04-26 20:10:36 +0300

    first commit

---
+++ web/index.php
@@ -0,0 +1,12 @@
+﻿<!DOCTYPE html>
+<html xmlns:og="http://ogp.me/ns#" prefix="og: http://ogp.me/ns#">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" media="all" href="/css/styles.css" />
+	<title>А</title>
+</head>
+<body>
+	<h1>A</h1>
+</body>
+</html>
